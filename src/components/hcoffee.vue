921ac81d71107a4d9a3f9e91d20495c5c1398f7b
tsuyoshi007 <45678324+tsuyoshi007@users.noreply.github.com>
2019-03-05 20:32:25 +0700

    organize project

---
+++ src/components/hcoffee.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="#app"></div>
+</template>
+<script>
+import style from "../assets/style.scss";
+export default {
+  name: "main"
+};
+</script>
+
+<style lang="sass">
+    #app {
+        background-color: bg;
+        width:100%;
+        height:100%;
+        position: absolute;
+    }
+</style>
+
